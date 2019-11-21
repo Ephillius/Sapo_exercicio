2868b329b1cee547162555c9152a06287722db91
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-11-21 05:13:50 +0000

    Add changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,24 +1,21 @@
-var navButton = document.querySelector("#btnOpen");
-
-document.querySelector("#right").addEventListener("click", () => {
-	for (let i = 0; i < geometryList.length; i++) {
-		geometryList[i].classList.add("teal");
-	}
-});
+var navButton = document.getElementById("btnOpen");
+var coll = document.querySelector(".collapse");
 
 
+
+//TOGGLE NAV//
 
 function toggle(e) {
 	e.classList.toggle("switch");
 }
 
-function toggleClass(el, className) {
-	var el = document.querySelector(el);
+function toggleClass(e, className) {
+	var e = document.querySelector(e);
 
-	if (el.classList) {
-		el.classList.toggle(className);
+	if (e.classList) {
+		e.classList.toggle(className);
 	} else {
-	    var classes = el.className.split(' ');
+	    var classes = e.className.split(' ');
 	    var existingIndex = -1;
 	    for (var j = classes.length; j--;) {
 	    	if (classes[j] === className) {
@@ -32,7 +29,7 @@ function toggleClass(el, className) {
 	    	classes.push(className);
 	    }
 
-		el.className = classes.join(' ');
+		e.className = classes.join(' ');
 	}
 }
 
@@ -42,3 +39,75 @@ navButton.addEventListener("click", () => {
 	toggleClass("#header", "slide");
 	toggleClass(".header-image", "invis");
 });
+
+
+//COLORS//
+
+document.getElementById("right").addEventListener("click", () => {
+	for (let i = 0; i < geometryList.length; i++) {
+		geometryList[i].classList.add("teal");
+	}
+});
+
+
+//COLLAPSE//
+
+coll.addEventListener("click", function() {
+	this.classList.toggle("active");
+	var content = this.previousElementSibling;
+	if (content.style.display === "block") {
+		content.style.display = "none";
+	} else {
+		content.style.display = "block";
+	}
+});
+
+
+//FROG LEAPS//
+
+function solution(x, y, d) {
+	var jumps;
+	if (x < 1 || y < 1 || d < 1) {
+		alert('Above 1, please.');
+	}
+	if (x > 1000000000 || y > 1000000000 || d > 1000000000) {
+		alert('Way too damn high!');
+	}
+
+	if ( (y-x) < d ) {
+		jumps = 1;
+	} else {
+		if ( (y-x) % d === 0) {
+			jumps = parseInt((y-x)/d);
+		} else {
+			jumps = parseInt(((y-x)/d)+1);
+		}
+	}
+
+	return jumps.toString();
+}
+
+
+//ONLY NUMBERS//
+
+function isNumber(e) {
+    e = (e) ? e : window.event;
+    var charCode = (e.which) ? e.which : e.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+
+//CALC NUMB JUMPS//
+
+
+document.querySelector('#total').addEventListener("click", (e) => {
+	var x = parseInt(document.querySelector('#x').value);
+	var y = parseInt(document.querySelector('#y').value);
+	var d = parseInt(document.querySelector('#d').value);
+
+	e.preventDefault();
+	document.querySelector("#number").innerHTML = "deu " + solution(x, y, d) + " saltos";
+}, false);
