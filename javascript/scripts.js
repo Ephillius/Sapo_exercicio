1a5859562636b9f5184c356c559d2aac3be8f6bd
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-11-25 00:40:06 +0000

    Add changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -61,7 +61,7 @@ document.getElementById("left").addEventListener("click", () => {
 
 coll.addEventListener("click", function() {
 	this.classList.toggle("active");
-	var content = this.previousElementSibling;
+	var content = document.querySelector(".mission");
 	if (content.style.display === "block") {
 		content.style.display = "none";
 	} else {
